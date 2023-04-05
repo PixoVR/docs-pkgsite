58eb3ef877d8930d4259a5ccc022656c7c678451
Chris Healer <chris@eyelash.ai>
2023-04-05 10:34:49 -0600

    go?

--- setup_pkgsite.sh
+++ setup_pkgsite.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"cd $DIR
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
 
 # exit on errors
 set -e
 
-echo "setup pkgsite..."
+#setup pkgsite
+git clone https://github.com/golang/pkgsite.git
+cd pkgsite
+#go mod tidy
+#go install ./cmd/pkgsite
+go version
 
 echo "link pkgsite executable..."
 
