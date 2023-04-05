456733bd8125515aa4707ef7cc67c7228f7ae6b5
Chris Healer <chris@eyelash.ai>
2023-04-05 12:07:03 -0600

    version

--- setup_pkgsite.sh
+++ setup_pkgsite.sh
@@ -10,8 +10,7 @@ set -e
 git clone https://github.com/golang/pkgsite.git
 cd pkgsite
 go install ./cmd/pkgsite
-go version
 
-echo "link pkgsite executable..."
+echo "pkgsite version: `pkgsite -v`"
 
 echo "other change..."
