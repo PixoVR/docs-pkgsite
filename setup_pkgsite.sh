285bc38ad2a46894a440a19e16f6b990d7199ad8
Chris Healer <chris@eyelash.ai>
2023-04-14 16:30:13 -0600

    startup scripts

--- setup_pkgsite.sh
+++ setup_pkgsite.sh
@@ -6,9 +6,13 @@ cd $DIR
 # exit on errors
 set -e
 
-#setup pkgsite
+# get pkgsite
 git clone --recursive https://github.com/golang/pkgsite.git
+
+# setup pkgsite and cosmetics
 cd pkgsite
 #./all.bash build_static #build static site, with cosmetic changes
 go install ./cmd/pkgsite
 
+# prep for startup later
+chmod 755 ./*
