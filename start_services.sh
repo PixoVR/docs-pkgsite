cbfd58733acfac190fd762e872eb9b7f4fbd63a9
Chris Healer <chris@eyelash.ai>
2023-04-16 16:26:21 -0600

    tweaks

--- start_services.sh
+++ start_services.sh
@@ -35,10 +35,10 @@ sleep 5
 
 echo "Pulling $PROJECT_URL/builtin"
 	#curl "http://localhost:3000/$PROJECT_URL/" 2>&1 > /dev/null
-#curl "http://localhost:3000/$PROJECT_URL/builtin#string" &>/dev/null
+curl "http://localhost:3000/$PROJECT_URL/builtin#string" &>/dev/null
 	#curl "http://localhost:3000/$PROJECT_URL/builtin#string" > /dev/null
 	#curl "http://localhost:3000/$PROJECT_URL/builtin#string"
-curl "http://localhost:3001/builtin#string"
+#curl "http://localhost:3001/builtin#string"
 
 # Wait for any process to exit
 wait -n
