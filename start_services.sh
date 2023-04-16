5451dc1eefba091c5a53e999cd7d3f5856cf5de3
Chris Healer <chris@eyelash.ai>
2023-04-15 21:11:43 -0600

    tweaks

--- start_services.sh
+++ start_services.sh
@@ -9,6 +9,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
+# get env set up
+source /env2.sh
+
 # Move gopath stuff to a writable place
 # this has to happen in the actual instance running,
 # because /tmp is ephemeral and writable, but empty
