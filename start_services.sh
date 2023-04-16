35f908a00ae838bfd1000e928b31b29e7f72a964
Chris Healer <chris@eyelash.ai>
2023-04-16 12:17:08 -0600

    tweaks

--- start_services.sh
+++ start_services.sh
@@ -41,12 +41,12 @@ echo Starting pkgsite: $PKGSITE_HOST:$PKGSITE_PORT for $PKGSITE_SOURCE
 ./start_pkgsite.sh &
 
 # pull an external library during build, so it's cached
-sleep 120
-echo "Pulling $PROJECT_URL/builtin"
-#curl "http://localhost:3000/$PROJECT_URL/" 2>&1 > /dev/null
-curl "http://localhost:3000/$PROJECT_URL/builtin#string" &>/dev/null
-#curl "http://localhost:3000/$PROJECT_URL/builtin#string" > /dev/null
-#curl "http://localhost:3000/$PROJECT_URL/builtin#string"
+#sleep 120
+#echo "Pulling $PROJECT_URL/builtin"
+	#curl "http://localhost:3000/$PROJECT_URL/" 2>&1 > /dev/null
+#curl "http://localhost:3000/$PROJECT_URL/builtin#string" &>/dev/null
+	#curl "http://localhost:3000/$PROJECT_URL/builtin#string" > /dev/null
+	#curl "http://localhost:3000/$PROJECT_URL/builtin#string"
 
 # Wait for any process to exit
 wait -n
