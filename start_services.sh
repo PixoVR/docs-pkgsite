21ac16fe59af654da2c2edfb7156841017371485
Chris Healer <chris@eyelash.ai>
2023-04-16 04:50:52 -0600

    tweaks

--- start_services.sh
+++ start_services.sh
@@ -42,7 +42,7 @@ echo Starting pkgsite: $PKGSITE_HOST:$PKGSITE_PORT for $PKGSITE_SOURCE
 sleep 60
 echo "Pulling $PROJECT_URL/builtin"
 #curl "http://localhost:3000/$PROJECT_URL/" 2>&1 > /dev/null
-curl "http://localhost:3000/$PROJECT_URL/builtin#string" 2>&1 > /dev/null
+curl "http://localhost:3000/$PROJECT_URL/builtin#string" &>/dev/null
 #curl "http://localhost:3000/$PROJECT_URL/builtin#string" > /dev/null
 #curl "http://localhost:3000/$PROJECT_URL/builtin#string"
 
